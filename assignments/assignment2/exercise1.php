240faa1b804ccf03194e60dfc654cb5f6d4c3128
Carmen Samaniego <csamanicg@wccnet.edu>
2022-09-30 14:00:26 -0400

    updates

---
+++ assignments/assignment2/exercise1.php
@@ -0,0 +1,37 @@
+<?<php>
+
+   class Sets {
+        public function numberSets(){
+            for ($i = 0; $i < 10; $i++) {
+                if ($i % 2 == 0){
+                continue;
+                }
+                echo "$i<br>";
+            }
+            
+                
+        }
+    }
+
+    $set = new Sets;
+    $display = $set->numberSets();
+
+</php>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>MySQL PHP Test</title>
+</head>
+<body class="container">
+    <?php  
+        echo $display; 
+    ?>
+</body>
+</html>
